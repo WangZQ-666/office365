de1400b3b14ce9223a694e9d3462056fca14a71f
WangZQ-666 <133641115+WangZQ-666@users.noreply.github.com>
2023-05-15 16:52:10 +0800

    Add files via upload

---
+++ TxtXQ/main.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+
+firstdir = 'D:/Desktop/ex/'  # 文件原路径
+tardir = 'D:/Desktop/'  # 文件目标路径
+pathdir = os.listdir(firstdir)  # 获取原路径下的所有文件
+
+path = []
+for path1 in pathdir:
+    path.append(firstdir + path1)
+
+for file in path:
+    cut1 = file.split('.')
+    if cut1[-1] == 'txt':
+        cut2 = file.split('/')
+        shutil.move(file, tardir + '/' + cut2[-1])
